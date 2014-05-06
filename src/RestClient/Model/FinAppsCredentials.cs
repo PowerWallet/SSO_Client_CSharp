c82653ef5be809b615ebf6401bf8884f640b1618
Erich <qbantek@gmail.com>
2014-05-06 14:09:13 -0400

    moved some logic ..

---
+++ src/RestClient/Model/FinAppsCredentials.cs
@@ -0,0 +1,8 @@
+namespace FinApps.SSO.RestClient.Model
+{
+    public class FinAppsCredentials
+    {
+        public string Email { get; set; }
+        public string FinAppsUserToken { get; set; }
+    }
+}
---
+++ src/RestClient/Model/NewUserResponse.cs
@@ -0,0 +1,10 @@
+using FinApps.SSO.RestClient.Annotations;
+
+namespace FinApps.SSO.RestClient.Model
+{
+    [UsedImplicitly]
+    public class NewUserResponse
+    {
+        public string UserToken { get; [UsedImplicitly] set; }
+    }
+}
