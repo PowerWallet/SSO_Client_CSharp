a0887628996268d9318cde56ce070778f22bc61d
Erich <qbantek@gmail.com>
2014-05-07 11:19:25 -0400

    missing files => visual studio

---
+++ src/MVC5/Filters/LogFilterAttribute.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Web.Mvc;
+using NLog;
+
+namespace FinApps.SSO.MVC5.Filters
+{
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, Inherited = true, AllowMultiple = true)]
+    public class LogAttribute : ActionFilterAttribute
+    {
+        private static readonly Logger Logger = LogManager.GetCurrentClassLogger();
+
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            var rd = filterContext.RequestContext.RouteData;
+            string currentAction = rd.GetRequiredString("action");
+            string currentController = rd.GetRequiredString("controller");
+
+            Logger.Debug("{0}.{1} => Executing", currentController, currentAction);
+        }
+
+        public override void OnActionExecuted(ActionExecutedContext filterContext)
+        {
+            var rd = filterContext.RequestContext.RouteData;
+            string currentAction = rd.GetRequiredString("action");
+            string currentController = rd.GetRequiredString("controller");
+
+            Logger.Debug("{0}.{1} => Executed", currentController, currentAction);
+        }
+    }
+}
