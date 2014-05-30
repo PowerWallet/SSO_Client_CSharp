92e8f97333a06b26c3fdcd045bb492a36487f778
Erich <qbantek@gmail.com>
2014-05-30 16:33:16 -0400

    remove unused classes.

--- src/RestClient_Base/Model/FinAppsBase.cs
+++ src/RestClient_Base/Model/FinAppsBase.cs
@@ -4,12 +4,7 @@ namespace FinApps.SSO.RestClient_Base.Model
 {
     public abstract class FinAppsBase
     {
+        // ReSharper disable once UnusedAutoPropertyAccessor.Global
         public List<FinAppsError> Errors { get; set; }
-    }
-
-    public class FinAppsError
-    {
-        public string PropertyName { get; set; }
-        public string ErrorMessage { get; set; }
     }
 }
---
+++ src/RestClient_Base/Model/FinAppsError.cs
@@ -0,0 +1,8 @@
+﻿namespace FinApps.SSO.RestClient_Base.Model
+{
+    public class FinAppsError
+    {
+        public string PropertyName { get; set; }
+        public string ErrorMessage { get; set; }
+    }
+}
