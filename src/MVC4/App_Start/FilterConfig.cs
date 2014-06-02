830c002f0a2a4e96ca4e802a2017c7c7c99d04d4
Erich <qbantek@gmail.com>
2014-06-02 17:20:01 -0400

    wip

--- src/MVC4/App_Start/FilterConfig.cs
+++ src/MVC4/App_Start/FilterConfig.cs
@@ -7,7 +7,7 @@ namespace FinApps.SSO.MVC4
     {
         public static void RegisterGlobalFilters(GlobalFilterCollection filters)
         {
-            filters.Add(new HandleErrorAttribute());
+            //filters.Add(new HandleErrorAttribute());
             filters.Add(new InitializeSimpleMembershipAttribute());
         }
     }
