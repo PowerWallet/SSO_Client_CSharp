7676b62c63588b9a9e68c0c9863d0b52af382c06
Erich <qbantek@gmail.com>
2014-06-02 17:48:32 -0400

    wip

--- src/MVC4/Global.asax.cs
+++ src/MVC4/Global.asax.cs
@@ -1,7 +1,14 @@
-﻿using System.Web.Http;
+﻿using System;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Threading;
+using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using FinApps.SSO.MVC4.Models;
+using NLog;
+using WebMatrix.WebData;
 
 namespace FinApps.SSO.MVC4
 {
@@ -10,6 +17,41 @@ namespace FinApps.SSO.MVC4
 
     public class MvcApplication : System.Web.HttpApplication
     {
+        private static readonly Logger logger = LogManager.GetCurrentClassLogger();
+        
+        private static SimpleMembershipInitializer _initializer;
+        private static object _initializerLock = new object();
+        private static bool _isInitialized;
+
+        private class SimpleMembershipInitializer
+        {
+            public SimpleMembershipInitializer()
+            {
+                Database.SetInitializer<UsersContext>(null);
+
+                try
+                {
+                    using (var context = new UsersContext())
+                    {
+                        if (!context.Database.Exists())
+                        {
+                            logger.Info("Creating SimpleMembership database without Entity Framework migration schema");
+                            // Create the SimpleMembership database without Entity Framework migration schema
+                            ((IObjectContextAdapter)context).ObjectContext.CreateDatabase();
+                        }
+                    }
+
+                    logger.Info("Initialize Database Connection");
+                    WebSecurity.InitializeDatabaseConnection("DefaultConnection", "UserProfile", "UserId", "UserName", autoCreateTables: true);
+                }
+                catch (Exception ex)
+                {
+                    logger.Error(ex.Message);
+                    throw new InvalidOperationException("The ASP.NET Simple Membership database could not be initialized. For more information, please see http://go.microsoft.com/fwlink/?LinkId=256588", ex);
+                }
+            }
+        }
+
         protected void Application_Start()
         {
             AreaRegistration.RegisterAllAreas();
@@ -19,6 +61,9 @@ namespace FinApps.SSO.MVC4
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
             AuthConfig.RegisterAuth();
+
+            // Ensure ASP.NET Simple Membership is initialized only once per app start
+            LazyInitializer.EnsureInitialized(ref _initializer, ref _isInitialized, ref _initializerLock);
         }
     }
 }
